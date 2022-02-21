876178b598cf2ee763ece431a3763aaa3afe4af9
jihoon-jang <jigns153@naver.com>
2022-02-22 01:01:18 +0900

    2022/02/22

---
+++ BJ_2606.kt
@@ -0,0 +1,54 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+val br = BufferedReader(InputStreamReader(System.`in`))
+var stz = StringTokenizer("")
+val computerCount = br.readLine().toInt()
+val network = br.readLine().toInt()
+val infection = Array(computerCount + 1) { false }
+val connection = Array(computerCount + 1) { ArrayList<Int>() }
+val queue = LinkedList<Int>()
+
+fun main() {
+    for (i in 0 until network) {
+        stz = StringTokenizer(br.readLine())
+        val computer1 = stz.nextToken().toInt()
+        val computer2 = stz.nextToken().toInt()
+
+        connection[computer1].add(computer2)
+        connection[computer2].add(computer1)
+    }
+
+    for (computerNo in connection[1]) {
+        queue.push(computerNo)
+    }
+    infection[1] = true
+    infect()
+    print(count() - 1)
+}
+
+fun infect() {
+    while (queue.isNotEmpty()) {
+        val computerNo = queue.poll()
+
+        if (!infection[computerNo]) {
+            infection[computerNo] = true
+
+            for (nextComputer in connection[computerNo]) {
+                queue.push(nextComputer)
+            }
+        }
+    }
+}
+
+fun count(): Int {
+    var count = 0
+
+    for (i in infection) {
+        if (i) {
+            count++
+        }
+    }
+    return count
+}
